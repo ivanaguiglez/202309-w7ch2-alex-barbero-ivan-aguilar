576326b13283326e81ed784b37fee92e4456cec8
Alex Barbero <barrufat@hotmail.com>
2023-10-26 08:24:45 +0200

    Add App styles and fonts

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { Character, CharactersResponse } from "../../types";
+import { useEffect, useState } from "react";
+import { Character } from "../../types";
 import CharacterCard from "../CharacterCard/CharacterCard";
 
 import "./App.css";
@@ -8,29 +8,38 @@ const App = () => {
   const [characters, setCharacters] = useState<Character[]>([]);
 
   const getCharacters = async (): Promise<void> => {
-    const charactersUrl = "https://swapi.dev/api/people";
+    const charactersUrl =
+      "https://starwars-characters-api-qcun.onrender.com/characters";
 
     try {
       const response = await fetch(charactersUrl);
-      const charactersData = (await response.json()) as CharactersResponse;
+      const charactersData = (await response.json()) as Character[];
 
-      setCharacters(charactersData.results);
+      setCharacters(charactersData);
     } catch {
       throw new Error("Error fetching data from Star Wars API");
     }
   };
 
-  getCharacters();
+  useEffect(() => {
+    getCharacters();
+  }, []);
 
   return (
     <div className="main-container">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/2560px-Star_Wars_Logo.svg.png"
+        alt="star wars main title"
+        width="300"
+        height="200"
+      />
       <ul className="characters">
-        {characters.map((character, id) => (
-          <li key={id + 1}>
+        {characters.map((character, position) => (
+          <li className="characters__item" key={position + 1}>
             <CharacterCard
               character={character}
               picture={`https://starwars-visualguide.com/assets/img/characters/${
-                id + 1
+                position + 1
               }.jpg`}
             />
           </li>
